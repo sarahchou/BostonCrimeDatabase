c51d4571c263cc7e6153f8fe364e4d1c0f5bd4dc
sarahchou <chou.s@husky.neu.edu>
2018-11-12 18:08:55 -0500

    sarahs test file - do not merge

--- database/test.sql
+++ database/test.sql
@@ -4,17 +4,17 @@ use mydb;
 
 
 
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\incidents_cleaned_v3_pt_1.csv' INTO TABLE incident
+LOAD DATA INFILE '/Users/schou/Desktop/BostonCrimeDatabase/database/incidents_cleaned_v3/incidents_cleaned_v3_pt_1.csv' INTO TABLE incident
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
   
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\incidents_cleaned_v3_pt2.csv' INTO TABLE incident
+LOAD DATA INFILE '/Users/schou/Desktop/BostonCrimeDatabase/database/incidents_cleaned_v3/incidents_cleaned_v3_pt2.csv' INTO TABLE incident
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
   
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\incident_offence_cleaned.csv' INTO TABLE incident_offence
+LOAD DATA INFILE '/Users/schou/Desktop/BostonCrimeDatabase/database/incident_offence_cleaned.csv' INTO TABLE incident_offence
   CHARACTER SET latin1
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
